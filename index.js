d6062985384da8be86581683a8904d24dba9a353
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-02-27 18:04:30 +0100

    feat: created user model and migration

--- index.js
+++ index.js
@@ -9,50 +9,6 @@ app.use(express.json());
 
 const PORT = 3000;
 
-
-app.get('/welcome', (req, res) => {
-  return res.send("Bienvenido a mi app")
-})
-
-app.post('/users', async (req, res) => {
-  const { name, first_surname, last_surname, phone, email, address } = req.body;
-//Guardar información
-const newUser = {
-  name,
-  first_surname,
-  last_surname,
-  phone,
-  email,
-  address
-}
-const user = await User.create(newUser)
-
-  console.log(req.body)
-  return res.json(user)
-})
-
-app.get('/users', async(req,res) => {
-
-  const users= await User.findAll();
-  return res.json(users);
-})
-
-app.get("/users/:id", async (req,res) => {
-  const userId = req.params.id;
-  const user = await User.findByPk(userId)
-  return res.json(user);
-})
-
-app.delete('/users/:id', async(req,res) => {
-  const userId = req.params.id;
-  const deleteUser = await User.destroy ({
-    where: { id: userId }})
-    return res.json(deleteUser)
-
-  
-})
-
-
   db.then(() => {
     //Starting server
     app.listen(PORT, () => console.log("Server on port " + PORT));
---
+++ models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    first_surname: DataTypes.STRING,
+    second_surname: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address: DataTypes.STRING,
+    role_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
