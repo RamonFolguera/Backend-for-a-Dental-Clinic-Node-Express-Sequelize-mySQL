4a888f063cd55a4290b39c07ccbc53ce0cdac94a
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-03-01 22:51:22 +0100

    Fix:Fix some typing mistakes, added missing createdAt and updatedAt to the Doctors seeders

--- seeders/20230228081802-Doctor.js
+++ seeders/20230228081802-Doctor.js
@@ -5,8 +5,8 @@ module.exports = {
   async up (queryInterface, Sequelize) {
 
     await queryInterface.bulkInsert('Doctors', [
-      {id: 1,collegiate_num: 070706732, user_id:1},
-      {id: 2,collegiate_num: 085514682, user_id:3}
+      {id: 1,collegiate_num: 70706732, user_id:1, createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
+      {id: 2,collegiate_num: 85514682, user_id:3, createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"}
     ],{});
   },
 
--- seeders/20230228081804-Appointment.js
+++ seeders/20230228081804-Appointment.js
@@ -3,9 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    
     await queryInterface.bulkInsert('Appointments', [
       {id: 1,service_id: 1,user_id: 2, doctor_id: 1,comments: "First session",createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
-      {id: 2,service_id: 2,user_id: 2,doctor_id: 3,comments: "Second session",createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
+      {id: 2,service_id: 2,user_id: 2,doctor_id: 2,comments: "Second session",createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
     ],{});
   
   },
