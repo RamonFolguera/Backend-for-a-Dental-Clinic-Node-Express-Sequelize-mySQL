ffe27f4bff0c91fc9e504a24dcd0922eee10ba56
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-03-04 15:52:15 +0100

    fix: changed appointment route name

--- views/appointmentRoutes.js
+++ views/appointmentRoutes.js
@@ -4,7 +4,7 @@ const verifyToken = require('../middleware/verifyToken');
 
 const router = require('express').Router();
 
-router.get('/user', verifyToken, appointmentController.getAppointmentsByuserId);
+router.get('/mine', verifyToken, appointmentController.getAppointmentsByuserId);
 router.get('/', verifyToken, isDoctor, appointmentController.getAllAppointmentsAsDoctor);
 router.post('/', verifyToken, appointmentController.createAppointments);
 
